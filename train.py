da1b5adfbe0f0d240c010d85f27ffa078f2cf0f2
Duzeyao <330501241@qq.com>
2019-08-12 18:04:39 +0800

    Update train.py

--- train.py
+++ train.py
@@ -23,7 +23,8 @@ def build_files(data_path, tokenized_data_path, num_pieces, full_tokenizer, min_
         sublines = lines[all_len // num_pieces * i: all_len // num_pieces * (i + 1)]
         if i == num_pieces - 1:
             sublines.extend(lines[all_len // num_pieces * (i + 1):])  # 把尾部例子添加到最后一个piece
-        sublines = [full_tokenizer.tokenize(line) for line in sublines if len(line) > min_length]  # 只考虑长度超过min_length的句子
+        sublines = [full_tokenizer.tokenize(line) for line in sublines if
+                    len(line) > min_length]  # 只考虑长度超过min_length的句子
         sublines = [full_tokenizer.convert_tokens_to_ids(line) for line in sublines]
         full_line = []
         for subline in sublines:
@@ -99,7 +100,6 @@ def main():
     min_length = args.min_length
     output_dir = args.output_dir
     tb_writer = SummaryWriter(log_dir=args.writer_dir)
-   
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -166,7 +166,7 @@ def main():
                 samples.append(tokens[start_point: start_point + n_ctx])
                 start_point += stride
             start_point -= stride
-            last = tokens[start_point + n_ctx :]
+            last = tokens[start_point + n_ctx:]
             last.extend([full_tokenizer.convert_tokens_to_ids(['[PAD]']) * (n_ctx - len(last))])
             random.shuffle(samples)
             for step in range(len(samples) // batch_size):  # drop last
