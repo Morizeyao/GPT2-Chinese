ff60f752461ea13bced3a9861fb69cba0c43919d
Duzeyao <330501241@qq.com>
2019-08-18 15:45:19 +0800

    修复pytorch-transformers1.1兼容

--- tokenizations/tokenization_bert_without_wordpiece.py
+++ tokenizations/tokenization_bert_without_wordpiece.py
@@ -22,7 +22,7 @@ import os
 import unicodedata
 from io import open
 
-from pytorch_transformers.tokenization_utils import PreTrainedTokenizer, clean_up_tokenization
+from pytorch_transformers.tokenization_utils import PreTrainedTokenizer
 
 logger = logging.getLogger(__name__)
 
