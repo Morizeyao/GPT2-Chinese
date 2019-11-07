00bed53423b04c9bd5edaae11da8c66548b37911
Duzeyao <330501241@qq.com>
2019-11-07 15:05:57 +0800

    bugfix

--- train.py
+++ train.py
@@ -151,7 +151,7 @@ def main():
 
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
-        model = DataParallel(model)
+        model = DataParallel(model, device_ids=[int(i) for i in args.device.split(',')])
         multi_gpu = True
     print('starting training')
     overall_step = 0
