69dd4b3b170268b981820eb70d6401a02d81ff0f
Zeyao Du <ned1991@gmail.com>
2021-04-22 12:58:40 +0800

    发布新的易用版GPT2-Chinese

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import os
 import argparse
 from tqdm import trange
-from transformers import GPT2LMHeadModel
+from transformers import GPT2LMHeadModel, GPT2Config, BertTokenizer
 
 
 def is_word(word):
@@ -110,31 +110,22 @@ def fast_sample_sequence(model, context, length, temperature=1.0, top_k=30, top_
     return generate
 
 
-# 通过命令行参数--fast_pattern，指定模式
-def generate(n_ctx, model, context, length, tokenizer, temperature=1, top_k=0, top_p=0.0, repitition_penalty=1.0, device='cpu',
-             is_fast_pattern=False):
-    if is_fast_pattern:
-        return fast_sample_sequence(model, context, length, temperature=temperature, top_k=top_k, top_p=top_p,
-                                    device=device)
-    else:
-        return sample_sequence(model, context, length, n_ctx, tokenizer=tokenizer, temperature=temperature, top_k=top_k, top_p=top_p,
-                               repitition_penalty=repitition_penalty, device=device)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', default='0,1,2,3', type=str, required=False, help='生成设备')
+    parser.add_argument('--device', default='0', type=str, required=False, help='生成设备')
     parser.add_argument('--length', default=-1, type=int, required=False, help='生成长度')
     parser.add_argument('--batch_size', default=1, type=int, required=False, help='生成的batch size')
     parser.add_argument('--nsamples', default=10, type=int, required=False, help='生成几个样本')
     parser.add_argument('--temperature', default=1, type=float, required=False, help='生成温度')
     parser.add_argument('--topk', default=8, type=int, required=False, help='最高几选一')
     parser.add_argument('--topp', default=0, type=float, required=False, help='最高积累概率')
-    parser.add_argument('--model_config', default='config/model_config_small.json', type=str, required=False,
+    parser.add_argument('--model_config', default='config/model_config.json', type=str, required=False,
                         help='模型参数')
-    parser.add_argument('--tokenizer_path', default='cache/vocab_small.txt', type=str, required=False, help='词表路径')
-    parser.add_argument('--model_path', default='model/final_model', type=str, required=False, help='模型路径')
-    parser.add_argument('--prefix', default='萧炎', type=str, required=False, help='生成文章的开头')
+    parser.add_argument('--tokenizer_path', default='vocab/vocab.txt', type=str, required=False, help='词表路径')
+    parser.add_argument('--model_path', default='model/epoch=0-step=99.ckpt', type=str, required=False, help='模型路径')
+    parser.add_argument('--prefix', default='我', type=str, required=False, help='生成文章的开头')
     parser.add_argument('--no_wordpiece', action='store_true', help='不做word piece切词')
     parser.add_argument('--segment', action='store_true', help='中文以词为单位')
     parser.add_argument('--fast_pattern', action='store_true', help='采用更加快的方式生成文本')
@@ -144,11 +135,6 @@ def main():
 
     args = parser.parse_args()
     print('args:\n' + args.__repr__())
-
-    if args.segment:
-        from tokenizations import tokenization_bert_word_level as tokenization_bert
-    else:
-        from tokenizations import tokenization_bert
 
     os.environ["CUDA_VISIBLE_DEVICES"] = args.device  # 此处设置程序使用哪些显卡
     length = args.length
@@ -161,61 +147,21 @@ def main():
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    tokenizer = tokenization_bert.BertTokenizer(vocab_file=args.tokenizer_path)
-    model = GPT2LMHeadModel.from_pretrained(args.model_path)
+    tokenizer = BertTokenizer(vocab_file=args.tokenizer_path)
+    model_config = GPT2Config.from_json_file(args.model_config)
+    model = GPT2LMHeadModel(config=model_config)
+    state_dict = {key[6:] :value for key, value in torch.load(args.model_path, map_location='cpu')['state_dict'].items()}
+    model.load_state_dict(state_dict)
     model.to(device)
     model.eval()
 
-    n_ctx = model.config.n_ctx
 
-    if length == -1:
-        length = model.config.n_ctx
-    if args.save_samples:
-        if not os.path.exists(args.save_samples_path):
-            os.makedirs(args.save_samples_path)
-        samples_file = open(args.save_samples_path + '/samples.txt', 'w', encoding='utf8')
-    while True:
+    for i in range(10):
         raw_text = args.prefix
-        context_tokens = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(raw_text))
-        generated = 0
-        for _ in range(nsamples // batch_size):
-            out = generate(
-                n_ctx=n_ctx,
-                model=model,
-                context=context_tokens,
-                length=length,
-                is_fast_pattern=args.fast_pattern, tokenizer=tokenizer,
-                temperature=temperature, top_k=topk, top_p=topp, repitition_penalty=repetition_penalty, device=device
-            )
-            for i in range(batch_size):
-                generated += 1
-                text = tokenizer.convert_ids_to_tokens(out)
-                for i, item in enumerate(text[:-1]):  # 确保英文前后有空格
-                    if is_word(item) and is_word(text[i + 1]):
-                        text[i] = item + ' '
-                for i, item in enumerate(text):
-                    if item == '[MASK]':
-                        text[i] = ''
-                    elif item == '[CLS]':
-                        text[i] = '\n\n'
-                    elif item == '[SEP]':
-                        text[i] = '\n'
-                info = "=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40 + "\n"
-                print(info)
-                text = ''.join(text).replace('##', '').strip()
-                print(text)
-                if args.save_samples:
-                    samples_file.write(info)
-                    samples_file.write(text)
-                    samples_file.write('\n')
-                    samples_file.write('=' * 90)
-                    samples_file.write('\n' * 2)
-        print("=" * 80)
-        if generated == nsamples:
-            # close file when finish writing.
-            if args.save_samples:
-                samples_file.close()
-            break
+        encoded = tokenizer.encode_plus(raw_text)['input_ids']
+        out = sample_sequence(model, encoded, length=512, n_ctx=1024, tokenizer=tokenizer, temperature=temperature, top_k=topk, top_p=topp,
+                               repitition_penalty=repetition_penalty, device=device)
+        print(tokenizer.decode(out))
 
 
 if __name__ == '__main__':
